c772932ae4340d3985700290023e0a4a56172155
AHMAD MUSA <musa.ahmad4abubakar@gmail.com>
2025-10-22 20:07:53 +0100

    Initialize mpc_bev_sim module with metadata

---
+++ mpc_bev_sim/init.py
@@ -0,0 +1,10 @@
+"""
+mpc_bev_sim
+Model Predictive Control (MPC) path-tracking simulator with Battery Electric Vehicle (BEV) dynamics.
+"""
+
+from .support_files_car import SupportFilesCar  # convenience import
+
+__all__ = ["SupportFilesCar"]
+__version__ = "1.0.0"
+__author__ = "Ahmad A Musa"
